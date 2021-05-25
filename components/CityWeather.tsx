e5850651d23b2adfb86a7a0e2404449454889ed2
Alaa Alkhater <38783431+3la2ooh@users.noreply.github.com>
2021-05-25 14:48:33 +0300

    Added Navigation to City Page

--- components/CityWeather.tsx
+++ components/CityWeather.tsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { cities } from '../utils/Cities';
+
 function CityWeather(props: any) {
+	const selectedCity = cities.find((city) => city.key === props.route.params.key);
+
+	useEffect(() => {
+		props.navigation.setOptions({ title: selectedCity?.name });
+	}, []);
+
 	return (
 		<View>
 			<Text>City Weather</Text>
