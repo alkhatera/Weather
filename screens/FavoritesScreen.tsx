3f7888a3a998bae36d11f4e8f750073a23706c8d
Alaa Alkhater <38783431+3la2ooh@users.noreply.github.com>
2021-05-26 11:38:54 +0300

    Added a state where there are no favorites

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -3,17 +3,33 @@ import { StyleSheet, Text, View } from 'react-native';
 import { RootStateOrAny, useSelector } from 'react-redux';
 
 import SearchResults from '../components/SearchResults';
+import { City } from '../utils/Cities';
 
 function FavoritesScreen(props: any) {
-	const favortieCities = useSelector((state: RootStateOrAny) => state.cities.favoriteCities);
+	const favortieCities: City[] = useSelector(
+		(state: RootStateOrAny) => state.cities.favoriteCities
+	);
 
-	return (
+	return favortieCities.length <= 0 ? (
+		<View style={styles.container}>
+			<Text style={styles.text}>No cities were added to favorites 😓</Text>
+		</View>
+	) : (
 		<View>
 			<SearchResults cities={favortieCities} />
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	text: {
+		fontSize: 20,
+	},
+});
 
 export default FavoritesScreen;
